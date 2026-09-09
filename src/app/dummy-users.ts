28f4cf6c84247518ff060934e0a4c3ef72c243fb
Aleksandar Todorovic <doca88@live.com>
2026-09-09 21:35:00 +0200

    Input, output, signals, events...

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,22 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Danica Todorovic',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'Julija Todorovic',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Milica Todorovic',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'Aleksandar Todorovic',
+    avatar: 'user-4.jpg',
+  }
+];
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed, Input, input, Output, EventEmitter, output } from '@angular/core';
+//import {DUMMY_USERS} from '../dummy-users';
+
 
 @Component({
   selector: 'app-user',
@@ -7,7 +9,36 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent 
-{
+export class UserComponent {
 
+    //@Input({required: true}) avatar!: string; 
+    //@Input({required: true}) name!: string;
+
+    id = input<string>('');
+    avatar = input<string>('');
+    name = input<string>('');
+    path = computed(() => '/assets/' + this.avatar());
+
+    selectUserEvent = output<string>();
+  //@Output() select = new EventEmitter();
+  //randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  //selectedUser = signal(DUMMY_USERS[this.randomIndex]);
+
+  //path = computed(() => '/assets/' + this.selectedUser().avatar);
+  // get path() {
+  //   return '/assets/' + this.selectedUser().avatar;
+  // }
+
+  // get path() {
+  //   return '/assets/' + this.avatar();
+  // }
+  onSelectedUser() {
+    
+  this.selectUserEvent.emit(this.id());
+  
+    //console.log('Selected user:', this.selectedUser);
+    //const randomIndex1 = Math.floor(Math.random() * DUMMY_USERS.length);
+    //this.selectedUser.set(DUMMY_USERS[randomIndex1]);
+    //this.selectedUser = DUMMY_USERS[randomIndex1];
+  }
 }
